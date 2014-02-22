ad1a9b401d334a1e9182e996275536b64eece48d
Alex Bezuska <abezuska@gmail.com>
2014-02-21 22:58:14 -0500

    update manifest for chrome web store, add icons

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'bounds': {
-      'width': 640,
+      'width': 6,
       'height': 1136
     }
   });
